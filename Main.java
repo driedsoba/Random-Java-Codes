bd723a7f5ed57c37c3466c1031b1c841a56996d8
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-01-20 00:48:02 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,59 @@
-public class Main {
-    public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 4000;
-        int levelCompleted = 5;
-        int bonus = 100;
+import java.util.Scanner;
 
-        if(score < 5000 && score > 1000) {
-            System.out.println("Your score was less than 5000 but greater than 1000");
-        } else if (score < 1000){
-            System.out.println("Your score was less than 1000");
-        } else{
-            System.out.println("Got here");
+public class Main {
+
+    public static void main(String[] args) {
+        int currentYear = 2022;
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
         }
     }
+    public static String getInputFromConsole(int currentYear) {
+        String name = System.console().readLine("Hi, What is your Name? ");
+        System.out.println("Hi " + name + " , thanks for taking the course!");
+
+        String dateOfBirth = System.console().readLine("What year were you born in? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So you are "+ age + " years old";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+
+        Scanner scanner = new Scanner(System.in); // creating an object of type Scanner
+
+        System.out.println("Hi, What is your Name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name + " , thanks for taking the course!");
+
+        System.out.println("What year were you born in? ");
+
+        boolean validDOB = false;
+        int age = 0;
+
+        do {
+            System.out.println("Enter a year of birth >=" +
+                    (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters not allowed!!! Try again.");
+            }
+        } while (!validDOB);
+
+        return "So you are "+ age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+        return  (currentYear - dob);
+    }
+
 }
---
+++ Test.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        int num1, num2, num3, num4, num5, sum, product, min, max;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st integer: ");
+        num1 = sc.nextInt();
+
+        System.out.println("Enter 2nd integer: ");
+        num2 = sc.nextInt();
+
+        System.out.println("Enter 3rd integer: ");
+        num3 = sc.nextInt();
+
+        System.out.println("Enter 4th integer: ");
+        num4 = sc.nextInt();
+
+        System.out.println("Enter 5th integer: ");
+        num5 = sc.nextInt();
+
+        sum = num1 + num2 + num3 + num4 + num5;
+        product = num1 * num2 * num3 * num4 * num5;
+
+        min = num1;
+        max = num3;
+
+        if (num2 < num1) {        // Comparing to find min
+            min = num2;
+        } else if (num3 < num1) {
+            min = num3;
+        } else if (num4 < num1) {
+            min = num4;
+        } else if (num5 < num1) {
+            min = num5;
+        }
+
+        if (num1 > num3) {
+            max = num1;
+        } else if (num2 > num3) {
+            max = num2;
+        } else if (num4 > num3) {
+            max = num4;
+        } else if (num5 > num3) {
+            max = num5;
+        }
+
+        System.out.println("The sum is " + sum);
+        System.out.println("The product is " + product);
+        System.out.println("The min number is " + min);
+        System.out.println("The max number is " + max);
+    }
+}
