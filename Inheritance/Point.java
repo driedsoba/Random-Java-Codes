94068dde28710e0595d9a65d092cd9bbd3209bd5
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-01-20 01:00:26 +0800

    Add files via upload

---
+++ Inheritance/Point.java
@@ -0,0 +1,41 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return distance(0, 0);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(x - getX(), 2) + Math.pow(y - getY(), 2));
+    }
+
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+
+}
---
+++ Inheritance/Wall.java
@@ -0,0 +1,44 @@
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+    }
+    public Wall(double width, double height) {
+        this.width = width;
+        this.height = height;
+
+        if(this.width < 0) {
+            this.width = 0;
+        }
+        if (this.height < 0) {
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+        if (this.width < 0) {
+            this.width = 0;
+        }
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+        if (this.height < 0) {
+            this.height = 0;
+        }
+    }
+
+    public double getArea() {
+        return (this.height * this.width);
+    }
+}
