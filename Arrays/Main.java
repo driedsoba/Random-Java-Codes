94f3c538cb192b1d1c9719d0f7bd01b82ea90fd3
driedsoba <122796569+driedsoba@users.noreply.github.com>
2023-03-17 13:16:36 +0800

    Add files via upload

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -1,26 +1,47 @@
 package dev.lpa;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        int[] myIntArray = new int[10];
-        myIntArray[5] = 50;             // element 6
+        int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
 
-        double[] myDoubleArray = new double[10];
-        myDoubleArray[2] = 3.5;
-        System.out.println(myDoubleArray[2]);
+        int[] secondArray = new int[10];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray, 5);
+        System.out.println(Arrays.toString(secondArray));
 
-        // array initializer
-        int[] firstFivePositives = new int[]{1, 2, 3, 4, 5};
-        System.out.println("first = " + firstFivePositives[0]);
-        int arrayLen = firstFivePositives.length;
-        System.out.println("length of array = " + arrayLen);
-        System.out.println("last = " + firstFivePositives[arrayLen - 1]);
+        int[] thirdArray = getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
 
-        int[] newArray;
-        newArray = new int[]{5, 4, 3, 2, 1};
-        for(int i = 0; i < newArray.length; i++) {
-            System.out.println(newArray[i] + " ");
+        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+        int[] smallerArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArray = Arrays.copyOf(thirdArray, 15);
+        System.out.println(Arrays.toString(largerArray));
+    }
+
+    private static int[] getRandomArray(int len) {
+
+        Random random = new Random();
+        int[] newInt = new int[len];
+        for (int i = 0; i < len; i++) {
+            newInt[i] = random.nextInt(100);
         }
+
+        return newInt;
     }
 }
